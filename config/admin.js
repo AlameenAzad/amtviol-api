c28d69fc2702bf6956eebfcf1ea5572870f49f5c
AlameenAzad <alameen.azad@protonmail.com>
2022-04-04 19:21:04 +0300

    add config sync lib

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '41b0b6415fcf352d80417bfce27d7121'),
+    secret: env("ADMIN_JWT_SECRET", "41b0b6415fcf352d80417bfce27d7121"),
   },
+  watchIgnoreFiles: ["**/config/sync/**"],
 });
