a0914459ee34b11d4d017d3db27504d7673804c9
Alameen Azad <alameen.azad@protonmail.com>
2022-04-07 13:24:04 +0300

    project roles and visibility and view requests

--- src/api/project/controllers/project.js
+++ src/api/project/controllers/project.js
@@ -11,7 +11,7 @@ module.exports = createCoreController("api::project.project", ({ strapi }) => ({
     const entries = await strapi.entityService.findMany(
       "api::project.project",
       {
-        fields: ["title"],
+        fields: ["title", "visibility"],
         filters: {
           $or: [
             {
@@ -31,43 +31,76 @@ module.exports = createCoreController("api::project.project", ({ strapi }) => ({
         },
       }
     );
-    return entries;
+    var projects = entries.filter((project) => {
+      if (
+        project.visibility == "only for me" &&
+        project.owner.id == ctx.state.user.id
+      )
+        return project;
+      else if (project.visibility != "only for me") return project;
+    });
+    return projects;
   },
   async findOne(ctx) {
-    const entry = await strapi.entityService.findOne(
-      "api::project.project",
-      ctx.request.url.substr(14, 1),
+    var entry = await strapi.entityService.findMany("api::project.project", {
+      populate: {
+        owner: { fields: ["username"] },
+        editors: { fields: ["username"] },
+        readers: { fields: ["username"] },
+        categories: { fields: ["title"] },
+        tags: { fields: ["title"] },
+        info: "*",
+        details: "*",
+        estimatedCosts: "*",
+        links: "*",
+        uploads: "*",
+        fundingGuideline: "*",
+        municipality: "*",
+      },
+      filters: {
+        $or: [
+          {
+            owner: { id: ctx.state.user.id },
+          },
+          {
+            editors: { id: ctx.state.user.id },
+          },
+          {
+            readers: { id: ctx.state.user.id },
+          },
+          {
+            visibility: "all users",
+          },
+        ],
+        id: ctx.request.url.substr(14, 1),
+      },
+    });
+    entry.length == 0
+      ? ctx.unauthorized("You are not allowed to view this project details")
+      : (entry = entry[0]);
+    if (entry.visibility == "only for me" || entry.visibility == "listed only")
+      if (entry.owner.id == ctx.state.user.id) return this.getRequests(entry);
+      else
+        return ctx.unauthorized(
+          "You are not allowed to view this project due to visibility option"
+        );
+    else return this.getRequests(entry);
+  },
+  async getRequests(entry) {
+    const requests = await strapi.entityService.findMany(
+      "api::request.request",
       {
-        populate: {
-          owner: { fields: ["username"] },
-          editors: { fields: ["username"] },
-          readers: { fields: ["username"] },
-          categories: { fields: ["title"] },
-          tags: { fields: ["title"] },
-          info: "*",
-          details: "*",
-          estimatedCosts: "*",
-          links: "*",
-          uploads: "*",
-          fundingGuideline: "*",
-          municipality: "*",
-        },
+        fields: ["approved"],
         filters: {
-          $or: [
-            {
-              owner: { id: ctx.state.user.id },
-            },
-            {
-              editors: { id: ctx.state.user.id },
-            },
-            {
-              readers: { id: ctx.state.user.id },
-            },
-          ],
+          approved: false,
+        },
+        populate: {
+          user: { fields: "username" },
+          project: { fields: ["title"] },
         },
       }
     );
-
+    entry.requests = requests;
     return entry;
   },
 }));
