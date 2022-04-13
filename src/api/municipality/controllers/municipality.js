d138583315cb6a8205049ab5e3191e47c60a8195
Alameen Azad <alameen.azad@protonmail.com>
2022-04-13 15:36:39 +0300

    project count in municipality

--- src/api/municipality/controllers/municipality.js
+++ src/api/municipality/controllers/municipality.js
@@ -1,9 +1,27 @@
-'use strict';
+"use strict";
 
 /**
  *  municipality controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::municipality.municipality');
+module.exports = createCoreController(
+  "api::municipality.municipality",
+  ({ strapi }) => ({
+    async find(ctx) {
+      const entries = await strapi.entityService.findMany(
+        "api::municipality.municipality",
+        {
+          fields: ["title", "location"],
+          populate: { projects: true, profile: true },
+        }
+      );
+      entries.forEach((entry) => {
+        entry.projectsCount = entry.projects.length;
+        delete entry.projects;
+      });
+      return entries;
+    },
+  })
+);
