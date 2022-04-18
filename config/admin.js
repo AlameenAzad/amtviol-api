dd2ea075a6b538d5c23ff5d542a315d1db89741a
Alameen Azad <alameen.azad@protonmail.com>
2022-04-18 10:42:27 +0300

    added salt to config to prevent breakage

--- config/admin.js
+++ config/admin.js
@@ -2,5 +2,8 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env("ADMIN_JWT_SECRET", "41b0b6415fcf352d80417bfce27d7121"),
   },
+  apiToken: {
+    salt: env("API_TOKEN_SALT", "1dc8a1bbad00ab8215dbd1df112b7ad4"),
+  },
   watchIgnoreFiles: ["**/config/sync/**"],
 });
