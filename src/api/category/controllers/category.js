cc7098d3aa248015dd63e4ba8623798b5cfda4ce
Alameen Azad <alameen.azad@protonmail.com>
2022-04-27 14:41:06 +0300

    add data sets

--- src/api/category/controllers/category.js
+++ src/api/category/controllers/category.js
@@ -10,17 +10,26 @@ module.exports = createCoreController(
   "api::category.category",
   ({ strapi }) => ({
     async find(ctx) {
+      const role = ctx.state.user.role.type;
+      var filterObj = {
+        fields: ["title"],
+        populate: { projects: true, fundings: true, checklists: true },
+      };
+      if (role != "admin") delete filterObj.populate;
       const entries = await strapi.entityService.findMany(
         "api::category.category",
-        {
-          fields: ["title"],
-          populate: { projects: true },
-        }
+        filterObj
       );
-      entries.forEach((entry) => {
-        entry.projectsCount = entry.projects.length;
-        delete entry.projects;
-      });
+      if (role === "admin")
+        entries.forEach((entry) => {
+          entry.dataSet = {};
+          entry.dataSet.projects = entry.projects.length;
+          entry.dataSet.fundings = entry.fundings.length;
+          entry.dataSet.checklist = entry.checklists.length;
+          delete entry.projects;
+          delete entry.fundings;
+          delete entry.checklists;
+        });
       return entries;
     },
   })
--- src/api/tag/controllers/tag.js
+++ src/api/tag/controllers/tag.js
@@ -8,14 +8,26 @@ const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController("api::tag.tag", ({ strapi }) => ({
   async find(ctx) {
-    const entries = await strapi.entityService.findMany("api::tag.tag", {
+    const role = ctx.state.user.role.type;
+    var filterObj = {
       fields: ["title"],
-      populate: { projects: true },
-    });
-    entries.forEach((entry) => {
-      entry.projectsCount = entry.projects.length;
-      delete entry.projects;
-    });
+      populate: { projects: true, fundings: true, checklists: true },
+    };
+    if (role != "admin") delete filterObj.populate;
+    const entries = await strapi.entityService.findMany(
+      "api::tag.tag",
+      filterObj
+    );
+    if (role === "admin")
+      entries.forEach((entry) => {
+        entry.dataSet = {};
+        entry.dataSet.projects = entry.projects.length;
+        entry.dataSet.fundings = entry.fundings.length;
+        entry.dataSet.checklist = entry.checklists.length;
+        delete entry.projects;
+        delete entry.fundings;
+        delete entry.checklists;
+      });
     return entries;
   },
 }));
