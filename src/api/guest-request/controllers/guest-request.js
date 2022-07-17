41005ca6cbda50c5cd3d918b91f0f0220ec290f8
Alameen Azad <alameen.azad@protonmail.com>
2022-07-17 16:04:36 +0300

    add guest request to join API

---
+++ src/api/guest-request/controllers/guest-request.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  guest-request controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::guest-request.guest-request');
