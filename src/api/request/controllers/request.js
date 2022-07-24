00af9589a42ada6c8df949a9e6b0e7377f251ea5
Alameen Azad <alameen.azad@protonmail.com>
2022-07-24 12:54:58 +0300

    accept view/edit requests

--- src/api/request/controllers/request.js
+++ src/api/request/controllers/request.js
@@ -8,42 +8,77 @@ const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController("api::request.request", ({ strapi }) => ({
   async update(ctx) {
-    const projectOwner = await strapi.entityService.findOne(
+    const request = await strapi.entityService.findMany(
       "api::request.request",
-      ctx.params.id,
       {
-        fields: ["approved"],
+        fields: ["approved", "type"],
+        filters: {
+          approved: false,
+          id: ctx.params.id,
+          $or: [
+            {
+              project: {
+                owner: ctx.state.user.id,
+              },
+            },
+            {
+              funding: {
+                owner: ctx.state.user.id,
+              },
+            },
+            {
+              checklist: {
+                owner: ctx.state.user.id,
+              },
+            },
+          ],
+        },
         populate: {
-          user: { fields: ["username"] },
-          project: {
-            fields: ["title"],
-            populate: { owner: { fields: ["username"] } },
-          },
+          user: { fields: "username" },
+          funding: { fields: ["title"] },
+          project: { fields: ["title"] },
+          checklist: { fields: ["title"] },
         },
       }
     );
-    if (projectOwner.project.owner.id == ctx.state.user.id) {
+    // return request;
+    if (request.length > 0) {
+      if (request[0].funding != null) this.acceptFunding(request[0]);
+      else if (request[0].project != null) this.acceptProject(request[0]);
+      else if (request[0].checklist != null) this.acceptChecklist(request[0]);
       const response = await super.update(ctx);
       return response;
     } else
-      return ctx.unauthorized(
-        `You are not authorized to edit this invitation.`
+      return ctx.unauthorized(`You are not authorized to accept this request.`);
+  },
+  async acceptFunding(request) {
+    if (request.type == "edit")
+      await strapi.db.connection.context.raw(
+        `INSERT INTO fundings_editors_links VALUES (${request.funding.id}, ${request.user.id});`
+      );
+    else if (request.type == "view")
+      await strapi.db.connection.context.raw(
+        `INSERT INTO fundings_readers_links VALUES (${request.funding.id}, ${request.user.id});`
       );
   },
-  async find(ctx) {
-    const entries = await strapi.entityService.findMany(
-      "api::request.request",
-      {
-        fields: ["approved"],
-        populate: {
-          user: { fields: ["username"] },
-          project: {
-            fields: ["title"],
-            populate: { owner: { fields: ["username"] } },
-          },
-        },
-      }
-    );
-    return entries;
+  async acceptProject(request) {
+    if (request.type == "edit")
+      await strapi.db.connection.context.raw(
+        `INSERT INTO projects_editors_links VALUES (${request.project.id}, ${request.user.id});`
+      );
+    else if (request.type == "view")
+      await strapi.db.connection.context.raw(
+        `INSERT INTO projects_readers_links VALUES (${request.project.id}, ${request.user.id});`
+      );
+  },
+  async acceptChecklist(request) {
+    if (request.type == "edit")
+      await strapi.db.connection.context.raw(
+        `INSERT INTO checklists_editors_links VALUES (${request.checklist.id}, ${request.user.id});`
+      );
+    else if (request.type == "view")
+      await strapi.db.connection.context.raw(
+        `INSERT INTO checklists_readers_links VALUES (${request.checklist.id}, ${request.user.id});`
+      );
   },
 }));
