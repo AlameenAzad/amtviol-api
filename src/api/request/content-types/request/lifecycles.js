fb9f6666bb10591641f8889013d33a745ea04594
AlameenAzad <alameen.azad@protonmail.com>
2022-07-29 13:58:28 +0300

    after a request to view/edit send email to owner

---
+++ src/api/request/content-types/request/lifecycles.js
@@ -0,0 +1,41 @@
+module.exports = {
+  async afterCreate(event) {
+    const { params } = event;
+
+    var type = null;
+    if (params.data.hasOwnProperty("project")) type = "project";
+    else if (params.data.hasOwnProperty("funding")) type = "funding";
+    else if (params.data.hasOwnProperty("checklist")) type = "checklist";
+
+    const document = await strapi.entityService.findOne(
+      `api::${type}.${type}`,
+      params.data[type].id,
+      {
+        fields: ["title"],
+        populate: {
+          owner: {
+            fields: ["username", "email"],
+            populate: {
+              user_detail: {
+                populate: { notifications: { populate: { email: "*" } } },
+              },
+            },
+          },
+        },
+      }
+    );
+    if (document.owner.user_detail.notifications.email.dataRequests == true) {
+      console.log(document.owner.user_detail.notifications);
+      const userRequesting = await strapi
+        .controller("api::user-detail.user-detail")
+        .find({ state: { user: { id: params.data.user.id } } });
+      await strapi.plugins["email"].services.email.send({
+        to: document.owner.email,
+        from: process.env.EMAIL_AUTH,
+        replyTo: process.env.EMAIL_AUTH,
+        subject: `New request to ${params.data.type} ${type}: ${document.title}`,
+        html: `${userRequesting.fullName} is requesting to ${params.data.type} your ${type}: ${document.title} `,
+      });
+    }
+  },
+};
