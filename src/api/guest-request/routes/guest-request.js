505ce3116ba4407cdaaf7bd794bb3b122af6cc34
AlameenAzad <alameen.azad@protonmail.com>
2022-08-16 17:01:35 +0300

    add rate limit

--- src/api/guest-request/routes/guest-request.js
+++ src/api/guest-request/routes/guest-request.js
@@ -1,9 +1,15 @@
-'use strict';
+"use strict";
 
 /**
  * guest-request router.
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter('api::guest-request.guest-request');
+module.exports = createCoreRouter("api::guest-request.guest-request", {
+  config: {
+    create: {
+      middlewares: ["plugin::users-permissions.rateLimit"],
+    },
+  },
+});
