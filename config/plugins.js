2e630a76cc333cbf77d2b8df2d7221afe665ac12
airotii <57942711+airotii@users.noreply.github.com>
2023-04-11 10:35:56 +0300

    changed-plugin-js

--- config/plugins.js
+++ config/plugins.js
@@ -4,17 +4,17 @@ module.exports = ({ env }) => ({
     config: {
       provider: "nodemailer",
       providerOptions: {
-        host: env("SMTP_HOST", "smtp.gmail.com"),
+        host: env("SMTP_HOST", "smtp.ionos.de"),
         port: env("SMTP_PORT", 587),
         auth: {
-          user: env("EMAIL_AUTH", "areenaram19@gmail.com"),
-          pass: env("EMAIL_PASS", "zuelniwlwzpovnbe"),
+          user: env("EMAIL_AUTH"),
+          pass: env("EMAIL_PASS"),
         },
         // ... any custom nodemailer options
       },
       settings: {
-        defaultFrom: env("DEF_FROM", "areenaram19@gmail.com"),
-        defaultReplyTo: env("DEF_REPLYTO", "areenaram19@gmail.com"),
+        defaultFrom: env("DEF_FROM", "Alameen.Azad@kruscheCompany.com"),
+        defaultReplyTo: env("DEF_REPLYTO", "Alameen.Azad@kruscheCompany.com"),
       },
     },
   },
