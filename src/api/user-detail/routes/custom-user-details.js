17693597d06b0b9db334a6d8123a304f447d38a9
Alameen Azad <alameen.azad@protonmail.com>
2024-03-26 13:17:25 +0300

    Refactor user-detail routes

--- src/api/user-detail/routes/custom-user-details.js
+++ src/api/user-detail/routes/custom-user-details.js
@@ -1,31 +1,26 @@
 module.exports = {
   routes: [
     {
-      // Path defined with a URL parameter
       method: "GET",
       path: "/user/transfer/:id",
       handler: "user-detail.transferData",
     },
     {
-      // Path defined with a URL parameter
       method: "GET",
       path: "/user/overview",
       handler: "user-detail.dataOverview",
     },
     {
-      // Path defined with a URL parameter
       method: "GET",
       path: "/user/notification",
       handler: "user-detail.notification",
     },
     {
-      // Path defined with a URL parameter
       method: "GET",
       path: "/stats",
       handler: "user-detail.statsAndArchive",
     },
     {
-      // Path defined with a URL parameter
       method: "GET",
       path: "/public/data",
       handler: "user-detail.publicData",
@@ -34,7 +29,6 @@ module.exports = {
       },
     },
     {
-      // Path defined with a URL parameter
       method: "PUT",
       path: "/upload/caption/:id",
       handler: "user-detail.updateFileCaption",
