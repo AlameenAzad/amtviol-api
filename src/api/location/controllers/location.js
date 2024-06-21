dc2b2608d969cf06846ec95ca6f6b39b319e01fd
Alameen Azad <alameen.azad@protonmail.com>
2024-06-21 12:12:58 +0300

    if admin, send all locations

--- src/api/location/controllers/location.js
+++ src/api/location/controllers/location.js
@@ -8,12 +8,19 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::location.location', ({ strapi }) =>({
   async findByMunicipality(ctx) {
+    console.log("🚀 ~ findByMunicipality ~ ctx:", ctx.state)
+    const isAdmin = ctx.state.user.role.type === 'admin';
     const { id } = ctx.params;
+    const filters = {
+      fields: ['title'],
+    };
+    if (!isAdmin) {
+      filters.filters = {
+        municipality: id,
+      };
+    }
     const locations = await strapi.entityService.findMany('api::location.location',
-      {
-        fields: ['title'],
-        filters: { municipality: id },
-      });
+      filters);
     return locations;
   }
 }));
